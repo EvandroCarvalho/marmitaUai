3b0d16c8aead01f4a9ba45978773fafc10c2691f
EvandroCarvalho <evandrovjunior@gmail.com>
2019-02-11 02:09:13 -0200

    correções e validações na action

---
+++ app/service/googleAPIService.js
@@ -0,0 +1,9 @@
+
+export const consultByAdress = async (address) => {
+    //TODO
+/*     let result = ''
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address.street}+${address.city}&key=AIzaSyBBnjp4mPMBFKOr65qoagqyO4w7ByInSl8`
+    const response = await fetch(url)
+    response.json()
+        .then(data => console.log(data)) */
+}
